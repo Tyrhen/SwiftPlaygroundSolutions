c1c9ad800cf57d800d2a2eb5c969a38ae6b4d5d2
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-12-01 17:52:58 -0500

    Creating a New Function

---
+++ LTC1/Functions/9_NewFunction.swift
@@ -0,0 +1,19 @@
+func turnRight(){
+    turnLeft()
+    turnLeft()
+    turnLeft()
+}
+
+moveForward()
+turnLeft()
+
+for i in 1...3{
+    moveForward()
+    turnRight()
+}
+
+moveForward()
+turnLeft()
+
+moveForward()
+toggleSwitch()
