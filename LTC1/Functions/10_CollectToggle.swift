92e20c3943b9a46322d08de1787638d3c27064ce
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-12-01 17:53:18 -0500

    Collect, Toggle, Repeat

---
+++ LTC1/Functions/10_CollectToggle.swift
@@ -0,0 +1,25 @@
+func gemSwitch(){
+    //complete consecutive collection/toggle sequence
+    moveForward()
+    collectGem()
+    moveForward()
+    toggleSwitch()
+}
+
+gemSwitch()
+
+moveForward()
+turnLeft()
+
+gemSwitch()
+
+moveForward()
+moveForward()
+turnLeft()
+
+gemSwitch()
+
+moveForward()
+turnLeft()
+
+gemSwitch()
