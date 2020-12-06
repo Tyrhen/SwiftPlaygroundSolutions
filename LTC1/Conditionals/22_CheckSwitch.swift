568c9d345350c18a51d22f06b646cab56af0afcb
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-12-05 19:55:18 -0500

    Checking for Switches

---
+++ LTC1/Conditionals/22_CheckSwitch.swift
@@ -0,0 +1,18 @@
+moveForward()
+moveForward()
+
+if isOnClosedSwitch{
+    toggleSwitch()
+}
+else{
+    moveForward()
+}
+
+moveForward()
+
+if isOnClosedSwitch{
+    toggleSwitch()
+}
+else{
+    moveForward()
+}
