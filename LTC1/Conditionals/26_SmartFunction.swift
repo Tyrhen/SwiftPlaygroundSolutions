bad06e5bd1efed4ea4a05fd07988fec2bc13d01f
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-12-06 21:35:00 -0500

    Defining Smarter Functions

---
+++ LTC1/Conditionals/26_SmartFunction.swift
@@ -0,0 +1,28 @@
+func collectOrToggle(){
+    for i in range 1...2{
+        moveForward()
+        moveForward()
+
+        if isOnClosedSwitch{
+            toggleSwitch()
+            
+        }else if isOnGem{
+            collectGem() 
+        }
+    }
+}
+
+collectOrToggle()
+turnLeft()
+
+moveForward()
+moveForward()
+turnLeft()
+
+collectOrToggle()
+turnRight()
+
+moveForward()
+turnRight()
+
+collectOrToggle()
