4293be7792ed53c94cab0e086ffddc2997e396f8
Tyrhen <66821494+Tyrhen@users.noreply.github.com>
2020-12-19 20:50:12 -0500

    Seeking Seven Gems

---
+++ LTC2/Variables/4_seekingSeven.swift
@@ -0,0 +1,13 @@
+var gemCounter = 0 
+
+while gemCounter < 7{
+    moveForward()
+    if isOnGem{
+        collectGem()
+        gemCounter+=1
+    }
+    if isBlocked{
+        turnRight()
+        turnRight()
+    }
+}
